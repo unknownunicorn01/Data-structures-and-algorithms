ee70f211410df2845f183f960df75dd53c7bf148
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2025-10-19 16:41:40 +0530

    Update a0_recursion.java

--- DSA basics/Recursion/a0_recursion.java
+++ DSA basics/Recursion/a0_recursion.java
@@ -1,1 +1,12 @@
-
+import java.util.*;
+public class a0_recursion{
+  public static void message(){
+    hello();
+  }
+  public static void hello(){
+    System.out.println("hellow world");
+  }
+  public static void main(String[] args){
+    message();
+  }
+}
