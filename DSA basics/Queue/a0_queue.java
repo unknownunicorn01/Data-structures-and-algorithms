e5c87a312d44450447039c4651a3a1c972b3acb5
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2026-05-09 21:50:23 +0530

    Update and rename a1_queue.java to a0_queue.java

---
+++ DSA basics/Queue/a0_queue.java
@@ -0,0 +1,7 @@
+import java.util.Queue;
+public class a1_queue{
+  public static void main(String[] args){
+    //bellow is the code to initialize queue in java
+    java.util.Queue<Integer> myQueue = new LinkedList<>();
+  }
+}
